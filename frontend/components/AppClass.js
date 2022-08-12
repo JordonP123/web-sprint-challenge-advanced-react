871e075874b8bbd89a53fb0e0edeb2c3c977be6b
JordonP123 <jordonaren@gmail.com>
2022-08-12 01:17:16 -0400

    getting somewhere :)

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,46 +1,80 @@
 import React from 'react'
 
-// Suggested initial states
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
-
-const initialState = {
-  message: initialMessage,
-  email: initialEmail,
-  index: initialIndex,
-  steps: initialSteps,
-}
-
 export default class AppClass extends React.Component {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
+  state = {
+  initialMessage: '',
+  initialEmail: '',
+  initialSteps: 0,
+  initialIndex: 4,
+  xValue: 0,
+  yValue: 0,
+  }
 
   getXY = () => {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
+    //gett coordinates based on index and set them to state in getNextIndex
+    console.log( )
   }
+    
 
   getXYMessage = () => {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
+    
   }
 
   reset = () => {
-    // Use this helper to reset all states to their initial values.
+    this.setState({
+        initialMessage: '',
+        initialEmail: '',
+        initialSteps: 0,
+        initialIndex: 4,
+        xvalue: 0,
+        yvalue: 0,
+    })
   }
 
   getNextIndex = (direction) => {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
-  }
+    if(direction === 'left' && this.state.initialIndex === 0) return this.state.initialIndex
+    if(direction === 'left' && this.state.initialIndex === 3) return this.state.initialIndex
+    if(direction === 'left' && this.state.initialIndex === 6) return this.state.initialIndex
+
+    if(direction === 'right' && this.state.initialIndex === 2) return this.state.initialIndex
+    if(direction === 'right' && this.state.initialIndex === 5) return this.state.initialIndex
+    if(direction === 'right' && this.state.initialIndex === 8) return this.state.initialIndex
+
+    if(direction === 'up' && this.state.initialIndex === 0) return this.state.initialIndex
+    if(direction === 'up' && this.state.initialIndex === 1) return this.state.initialIndex
+    if(direction === 'up' && this.state.initialIndex === 2) return this.state.initialIndex
+
+    if(direction === 'down' && this.state.initialIndex === 6) return this.state.initialIndex
+    if(direction === 'down' && this.state.initialIndex === 7) return this.state.initialIndex
+    if(direction === 'down' && this.state.initialIndex === 8) return this.state.initialIndex
+
+    if(direction === 'left') return this.state.initialIndex -1
+    if(direction === 'right')return  this.state.initialIndex +1
+    if(direction === 'up')return  this.state.initialIndex -3
+    if(direction === 'down')return  this.state.initialIndex +3
+    }
+
+  
 
   move = (evt) => {
     // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
+    // and change any states accordingly.}
+      
+      this.setState({...this.state, 
+        initialIndex : this.getNextIndex(evt.target.id),
+      })
+
+
   }
 
   onChange = (evt) => {
@@ -52,18 +86,19 @@ export default class AppClass extends React.Component {
   }
 
   render() {
+    console.log(this.getXY())
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
           <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="steps">You moved {this.state.initialSteps} times</h3>
         </div>
         <div id="grid">
           {
             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
+              <div key={idx} className={`square${idx === this.state.initialIndex ? ' active' : ''}`}>
+                {idx === this.state.initialIndex ? 'B' : null}
               </div>
             ))
           }
@@ -72,11 +107,11 @@ export default class AppClass extends React.Component {
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={this.move} id="left">LEFT</button>
+          <button onClick={this.move} id="up">UP</button>
+          <button onClick={this.move}  id="right">RIGHT</button>
+          <button onClick={this.move} id="down">DOWN</button>
+          <button onClick={this.reset} id="reset">reset</button>
         </div>
         <form>
           <input id="email" type="email" placeholder="type email"></input>
